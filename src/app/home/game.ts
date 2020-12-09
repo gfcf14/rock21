c4ce2ce291bbc75eaf4e6e53acbbe7697a716a3b
Carlos(gfcf14) <carloscuba014@gmail.com>
2020-12-09 16:05:21 -0500

    insert test character

--- src/app/home/game.ts
+++ src/app/home/game.ts
@@ -1,16 +1,24 @@
 import * as Phaser from 'phaser';
 
 export class GameScene extends Phaser.Scene {
+  character: Phaser.Physics.Arcade.Sprite;
+
   constructor() {
     super({ key: 'game' });
   }
 
   preload() {
+    this.load.setPath('../assets/');
 
+    this.load.image('character', 'character.png');
   }
 
   create() {
+    this.character = this.physics.add.sprite(400, 300, 'character');
 
+    if (this.character.body instanceof Phaser.Physics.Arcade.Body) {
+      this.character.body.setCollideWorldBounds(true);
+    }
   }
 
   update() {
